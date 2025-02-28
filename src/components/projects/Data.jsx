237b5984947b4ad843f52e37438beb37c7a5e285
CodeCraftman2 <moneyyst399@outlook.com>
2025-02-28 23:48:51 +0530

    Initial commit

---
+++ src/components/projects/Data.jsx
@@ -0,0 +1,40 @@
+import Project1 from "../../assets/project1.webp";
+import Project2 from "../../assets/project2.webp";
+import Project3 from "../../assets/project3.webp";
+
+
+export const projectsData = [
+    {
+        id: 1,
+        image: Project1,
+        title: "Multi-Cloud storage API",
+        category: "1",
+    },
+    {
+        id: 2,
+        image: Project2,
+        title: "Trading AI",
+        category: "2",
+    },
+    {
+        id: 3,
+        image: Project3,
+        title: "Website",
+        category: "3",
+    },
+];
+
+export const projectsNav = [
+    {
+        name: "All",
+    },
+    {
+        name: "1",
+    },
+    {
+        name: "2",
+    },
+    {
+        name: "3",
+    },
+];
