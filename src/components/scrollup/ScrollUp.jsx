b82875b0058e66599f8065d25545d0b5115c30d1
CodeCraftman2 <moneyyst399@outlook.com>
2025-03-14 16:40:12 +0530

    ScrollUP commit message

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './scrollup.css';
 import { HiOutlineArrowSmUp } from "react-icons/hi";
 
 const ScrollUp = () => {
-    window.addEventListener("scroll", function () {
-        const scrollUp = document.querySelector(".scrollup");
-        if(this.scrollY >= 560) scrollUp.classList.add("show-scroll");
-        else scrollUp.classList.remove("show-scroll");
-    });
-  
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollUp = document.querySelector(".scrollup");
+            if (window.scrollY >= 560) {
+                scrollUp.classList.add("show-scroll");
+            } else {
+                scrollUp.classList.remove("show-scroll");
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
     return (
-    <a href="#" className="scrollup"><HiOutlineArrowSmUp className="scrollup__icon"/></a>
-  );
-}
+        <button className="scrollup" onClick={scrollToTop}>
+            <HiOutlineArrowSmUp className="scrollup__icon" />
+        </button>
+    );
+};
 
 export default ScrollUp;
